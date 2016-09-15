e557e85829d38cfcd786561397121f9b8f02059a
charitha306 <charitha306@gmail.com>
2016-09-15 11:10:48 -0400

    cleaning up IU Sead Coud Search code

--- iu-sead-cloud-search/src/main/java/org/iu/sead/cloud/util/Constants.java
+++ iu-sead-cloud-search/src/main/java/org/iu/sead/cloud/util/Constants.java
@@ -26,19 +26,8 @@ import java.util.Properties;
 
 public class Constants {
 
-	public static String mongoHost;
-	public static int mongoPort;
-	public static String mongoOreHost;
-	public static int mongoOrePort;
-
-	public static String pdtDbName;
-	public static String metaDbName;
-	public static String oreDbName;
-	public static String repoName;
-
+    public static String repoName;
 	public static String pdtURL;
-
-	public static String successStage = "Success";
 
 	static {
 		try {
@@ -53,13 +42,6 @@ public class Constants {
 				.getResourceAsStream("./default.properties");
 		Properties props = new Properties();
 		props.load(inputStream);
-		mongoHost = props.getProperty("mongo.host", "localhost");
-		mongoPort = Integer.parseInt(props.getProperty("mongo.port", "27017"));
-		pdtDbName = props.getProperty("pdt.db.name", "sead-pdt");
-		metaDbName = props.getProperty("metadata.db.name", "sead-metadata");
-		mongoOreHost = props.getProperty("mongo.ore.host", "localhost");
-		mongoOrePort = Integer.parseInt(props.getProperty("mongo.ore.port", "27018"));
-		oreDbName = props.getProperty("ore.db.name", "sead-ore");
 		repoName = props.getProperty("repository.name", "sda");
 		pdtURL = props.getProperty("pdt.url");
 	}
